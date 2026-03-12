27ff9b3d7e1080b71c5d7845988008de08df9106
Samu31-67 <b2031slalarcon@pshs.edu.ph>
2026-03-13 07:20:30 +0800

    Update CS.py

--- CS.py
+++ CS.py
@@ -1,13 +1,13 @@
 subtotal = 0.00
 
 for i in range(1,4):
-print(f"Item {i}")
-while True:
+ print(f"Item {i}")
+ while True:
   price = float(input(f"Enter price for item {i}: "))
   quantity = int(input(f"Enter quantity for item {i}: "))
   break 
-        except ValueError:
-            print("Invalid input! Please enter numbers only (e.g., 50 or 10.5).")
+   except ValueError:
+    print("Invalid input! Please enter numbers only (e.g., 50 or 10.5).")
     item_total = price * quantity
     subtotal += item_total
 
@@ -17,3 +17,4 @@ final_total = subtotal - discount
 print(f"\nSubtotal: {subtotal:,.2f}")
 print(f"Discount: {discount:,.2f}")
 print(f"Final Total: {final_total:,.2f} pesos")
+
